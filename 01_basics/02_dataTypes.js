f573107491fd38e475d41be0dd986a2e5027f5bb
k1LLsh0t0 <avinash.singh9214@gmail.com>
2023-12-11 14:27:09 +0530

    dataypes with notes

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,16 @@
+"use strict" //treat all JS Code as newer version
+
+// alert( 3 + 3) // we are using nodejs, not browser
+
+//code readability should be high  
+
+
+//number
+//string => ""
+//boolean => true/false
+//null =>standalone value
+//undefined =>
+//object
+
+console.log(typeof null) //output is object
+console.log(typeof undefined) //output=>undefined 
