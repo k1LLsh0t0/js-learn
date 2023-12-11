7ccd4c454f35caeb0830af32fd81d02235d26cd6
k1LLsh0t0 <avinash.singh9214@gmail.com>
2023-12-11 20:14:03 +0530

    Maths class done

---
+++ 01_basics/05_number.js
@@ -0,0 +1,20 @@
+const score = 400
+console.log(score);
+
+const balance = new Number (100)
+console.log(balance)
+
+console.log(balance.toString().length);
+console.log(balance.toFixed(1));
+
+const otherNumber =123.8966
+console.log(otherNumber.toPrecision(3));
+
+const hundreds =1000000
+console.log (hundreds.toLocaleString('en-IN')); //INDIAN UNIT SYSTEM
+
+
+const min=10
+const max=20
+
+console.log(Math.floor(Math.random()*(max-min+1)+min));
