2744b922bbb5f228869018628a83d3a4012cf18f
k1LLsh0t0 <avinash.singh9214@gmail.com>
2023-12-11 19:59:54 +0530

    Strings

---
+++ 01_basics/04_string.js
@@ -0,0 +1,10 @@
+const name = "Avinash"
+const repoCount = 50
+
+console.log(`hello my name is ${name} nad my repo count is ${repoCount}`)
+
+const gameName = new String('AvinashSingh')
+
+//console.lo(gameName[0]);
+// console.log(gameName.__proto__); GIVES all functions of the String 
+
